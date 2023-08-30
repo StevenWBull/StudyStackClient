ca259aa9ab993b9bfb90db2745085502755f85f1
Steven Bull <51507134+StevenWBull@users.noreply.github.com>
2023-08-30 11:04:41 -0500

    add in a footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,21 @@
 import logo from '../../logo.svg';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Footer from '../Footer/Footer';
 import Landing from '../Landing/Landing';
 
 const App = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Landing />} />
-                <Route path="/login" element={<h1>Login</h1>} />
-                <Route path="/home" element={<h1>Home</h1>} />
-            </Routes>
-        </Router>
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Landing />} />
+                    <Route path="/login" element={<h1>Login</h1>} />
+                    <Route path="/home" element={<h1>Home</h1>} />
+                </Routes>
+            </Router>
+            <Footer />
+        </>
     );
 };
 
