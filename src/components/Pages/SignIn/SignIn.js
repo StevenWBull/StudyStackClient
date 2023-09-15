ee0fe77075b496a208f0fed9817562339f84a86c
Hyojin <parkthoven88@gmail.com>
2023-09-15 01:41:18 -0400

    build home page

---
+++ src/components/Pages/SignIn/SignIn.js
@@ -0,0 +1,45 @@
+// SignUp.js
+
+import React from 'react';
+import './SignIn.css'; // Import the CSS file
+import studyStackLogo from '../../Images/Study_Stack_Full_Color.png';
+
+const SignIn = () => {
+    return (
+        <div className="signin-container">
+            <div className="center-content">
+                <div className="logo">
+                    <img src={studyStackLogo} alt="Study Stack Logo" />
+                </div>
+                <div className="signin-box">
+                    <h1>Sign In</h1>
+                    <form className="center-form">
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label>
+                            <input
+                                type="text"
+                                id="username"
+                                name="username"
+                                required
+                                placeholder="Enter your username"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                name="password"
+                                required
+                                placeholder="Enter your password"
+                            />
+                        </div>
+                        <button type="submit">Sign In</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignIn;
---
+++ src/components/Styles/Container/Container.style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: white;
+`;
+
+export const HeaderContainer = styled.div`
+    background-color: #ffd700;
+    width: 100%;
+    height: 30%;
+`;
+
+export const LandingContainer = styled.div`
+    background-color: #ffd700;
+    width: 100%;
+    height: 30%;
+`;
