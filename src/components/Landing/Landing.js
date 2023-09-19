230d99df4f799cfc0e9284b94627a5a13a916986
Steven Bull <51507134+StevenWBull@users.noreply.github.com>
2023-09-19 17:27:46 -0500

    resolve merge conflicts

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,18 @@
+import './Landing.css';
+
+const Landing = () => {
+    return (
+        <div>
+            <h1>Landing</h1>
+            <h2>This is StudyStack</h2>
+            <div>
+                <li>
+                    <a href="/login">Login</a>
+                    <a href="/signup">Sign Up</a>
+                </li>
+            </div>
+        </div>
+    );
+};
+
+export default Landing;
